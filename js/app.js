1aaec0750a942348fbc573d2d85766a43626c067
Jeffrey <descanjeffrey@gmail.com>
2016-04-20 20:12:29 +0200

    Check if all fingers are extended

--- js/app.js
+++ js/app.js
@@ -21,8 +21,27 @@ var controller = Leap.loop(null, function (frame) {
             var oldHand = oldFrame.hand(hand.id);
 
             if (hand.type == "right" && hand.valid == true && oldHand.valid == true) {
-                if (movementHand.moveRight(hand.stabilizedPalmPosition[0], oldHand, controller)) {
-                    console.log("Rechts: ", hand.stabilizedPalmPosition[0], oldHand.stabilizedPalmPosition[0]);
+                // Alle fingers moeten uitgestrekt zijn voordat we iets gaan herkennen
+                if (movementHand.fingersExtended(hand.fingers)) {
+                    if (movementHand.moveRight(hand.stabilizedPalmPosition[0], oldHand, controller)) {
+                        console.log("Right: ", hand.stabilizedPalmPosition[0], oldHand.stabilizedPalmPosition[0]);
+                    } else if (movementHand.moveLeft(hand.stabilizedPalmPosition[0], oldHand, controller)) {
+                        console.log("Left: ", hand.stabilizedPalmPosition[0], oldHand.stabilizedPalmPosition[0]);
+                    }
+
+                    if (movementHand.moveUp(hand.stabilizedPalmPosition[1], oldHand, controller)) {
+                        console.log("Up: ", hand.stabilizedPalmPosition[1], oldHand.stabilizedPalmPosition[1]);
+                    } else if (movementHand.moveDown(hand.stabilizedPalmPosition[1], oldHand, controller)) {
+                        console.log("Down: ", hand.stabilizedPalmPosition[1], oldHand.stabilizedPalmPosition[1]);
+                    }
+
+                    if (movementHand.moveForward(hand.stabilizedPalmPosition[2], oldHand, controller)) {
+                        console.log("Forward: ", hand.stabilizedPalmPosition[2], oldHand.stabilizedPalmPosition[2]);
+                    } else if (movementHand.moveBackward(hand.stabilizedPalmPosition[2], oldHand, controller)) {
+                        console.log("Backward: ", hand.stabilizedPalmPosition[2], oldHand.stabilizedPalmPosition[2]);
+                    }
+                }else{ // Niet alle vingers zijn uitgestrekt
+                    console.log("Niet alle fingers zijn uitgestrekt");
                 }
             }
         }
--- js/helper/constants.js
+++ js/helper/constants.js
@@ -2,4 +2,6 @@
 const AVERAGE_FRAMES = 20;
 
 // Minimum movement a user has to perform, until an action happens
-const MIN_MOVEMENT = 4;
+const MIN_MOVEMENT          = 4; // General movement
+const MIN_MOVEMENT_FORWARD  = 2; // Specific forward
+const MIN_MOVEMENT_BACKWARD = 2; // Specific backward
